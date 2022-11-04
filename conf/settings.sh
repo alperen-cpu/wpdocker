18e724944e0e658e4ed07e9afefbeb6001d49ca5
alperen-cpu <alperensah@outlook.com>
2022-11-04 17:46:59 +0300

    minor improvement

---
+++ conf/settings.sh
@@ -0,0 +1,2 @@
+sed -i 's/listen.owner \= www-data/listen.owner \= nginx/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i 's/listen.group \= www-data/listen.group \= nginx/g' /usr/local/etc/php-fpm.d/www.conf
--- settings.sh
+++
@@ -1,2 +0,0 @@
-RUN sed -i 's/listen.owner \= www-data/listen.owner \= nginx/g' /usr/local/etc/php-fpm.d/www.conf
-RUN sed -i 's/listen.group \= www-data/listen.group \= nginx/g' /usr/local/etc/php-fpm.d/www.conf
